04b262308ba65f541d18425a6ed78e61dbdc91ed
Lisdey Echevarria <lisdeymar07@gmail.com>
2022-01-23 18:03:35 -0500

    first commit

---
+++ dos.py
@@ -0,0 +1,14 @@
+estudiantes = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+for clave, valor in estudiantes.items ():
+    print(clave, ":", valor)
+    
+
+
+
+
